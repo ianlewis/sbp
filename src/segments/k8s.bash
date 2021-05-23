2cf2035175c9c5e00d449f16716f3b5809a895b1
Anders Brujordet <anders@brujordet.no>
2021-05-23 10:43:21 +0200

    Update the k8s segment to work with recent kubectl (#113)
    * Update the k8s segment to work with recent kubectl
    
    * Adapt to docker hub rate limiting

--- src/segments/k8s.bash
+++ src/segments/k8s.bash
@@ -5,20 +5,15 @@ segments::k8s() {
   [[ -f "$KUBE_CONFIG" ]] || return 0
   context="$(sed -n 's/.*current-context: \(.*\)/\1/p' "$KUBE_CONFIG")"
   [[ -z "$context" ]] && return 0
-  user=${context##*/}
-  namespace_and_host=${context%/*}
-  host=${namespace_and_host##*/}
-  host=${host%:*}
-  namespace=${namespace_and_host%/*}
+  namespace="$(pcregrep -M -- "- context:\n(^\s\s\w*.*\n)*  name: ${context}" ${KUBE_CONFIG} | sed -n 's/ *namespace: \(\w*\)/\1/p')"
+  if [[ -z "$namespace" ]]; then
+    namespace='default'
+  fi
 
-  if [[ "${user,,}" == "${SEGMENTS_K8S_DEFAULT_USER,,}" ]]; then
-    if [[ "$SEGMENTS_K8S_HIDE_CLUSTER" -eq 1 ]]; then
-      segment="${namespace}"
-    else
-      segment="${host}:${namespace}"
-    fi
+  if [[ -z $namespace || "$SEGMENTS_K8S_HIDE_CLUSTER" -eq 1 ]]; then
+    segment="${context}"
   else
-    segment="${user}@${host}/${namespace}"
+    segment="${context}/${namespace}"
   fi
 
   print_themed_segment 'normal' "${segment,,}"
