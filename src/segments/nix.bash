4906b462ec89e27d838370d28ca207a4e6ce1309
Sandro <sandro.jaeckel@sap.com>
2022-03-16 09:11:39 +0100

    segment/nix: don't show when lorri is activated (#124)

--- src/segments/nix.bash
+++ src/segments/nix.bash
@@ -1,7 +1,9 @@
 #! /usr/bin/env bash
 
 segments::nix() {
-  if [[ -n "$IN_NIX_SHELL" ]]; then
+  # lorri also sets IN_NIX_SHELL but a real nix-shell sets bunch more variables we can use to differentiate the two
+  # and only display the segment when we are in a real nix-shell which opens a nested bash which can be closed
+  if [[ -n $IN_NIX_SHELL && -v name && -v system ]]; then
     print_themed_segment 'normal' "nix-shell"
   fi
 }
