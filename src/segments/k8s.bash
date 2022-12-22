663d2c5e5a15f8d776e5e108c94a8d94a4c7480f
Flying Lollipop <fllp@users.noreply.github.com>
2022-12-22 11:01:20 +0100

    Enhance K8s segment (#128)
    * segment/k8s: use canonical kubekonfig variable name
    
    This will use the canonical KUBECONFIG variable used
    to specify custom paths for the kubeconfig to kubectl
    
    * feat: add symbol for K8s segment
    
    * feat: make segment icon configurable
    
    also use 'helm' icon from Nerd Fonts by default

--- src/segments/k8s.bash
+++ src/segments/k8s.bash
@@ -1,19 +1,21 @@
 #! /usr/bin/env bash
-KUBE_CONFIG="${HOME}/.kube/config"
+[[ -z "$KUBECONFIG" ]] && KUBECONFIG="${HOME}/.kube/config"
 
 segments::k8s() {
-  [[ -f "$KUBE_CONFIG" ]] || return 0
-  context="$(sed -n 's/.*current-context: \(.*\)/\1/p' "$KUBE_CONFIG")"
+  [[ -f "$KUBECONFIG" ]] || return 0
+  context="$(sed -n 's/.*current-context: \(.*\)/\1/p' "$KUBECONFIG")"
   [[ -z "$context" ]] && return 0
-  namespace="$(pcregrep -M -- "- context:\n(^\s\s\w*.*\n)*  name: ${context}" ${KUBE_CONFIG} | sed -n 's/ *namespace: \(\w*\)/\1/p')"
+  namespace="$(pcregrep -M -- "- context:\n(^\s\s\w*.*\n)*  name: ${context}" ${KUBECONFIG} | sed -n 's/ *namespace: \(\w*\)/\1/p')"
   if [[ -z "$namespace" ]]; then
     namespace='default'
   fi
 
+  local segment_icon_char="${SEGMENTS_K8S_ICON:-⎈}"
+
   if [[ -z $namespace || "$SEGMENTS_K8S_HIDE_CLUSTER" -eq 1 ]]; then
-    segment="${context}"
+    segment="${segment_icon_char} ${context}"
   else
-    segment="${context}/${namespace}"
+    segment="${segment_icon_char} ${context}/${namespace}"
   fi
 
   print_themed_segment 'normal' "${segment,,}"
